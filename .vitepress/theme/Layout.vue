05a21795393b808938d77616c55a0d964404fc28
wulukewu <lukewu1015@gmail.com>
2026-02-04 00:45:42 +0800

    fix: custom layout

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useData } from "vitepress";
+import BlogTheme from "@sugarat/theme";
+
+const { Layout } = BlogTheme;
+const { frontmatter } = useData();
+</script>
+
+<template>
+  <Layout>
+    <template #doc-before>
+      <h1 v-if="frontmatter.title" class="page-title">
+        {{ frontmatter.title }}
+      </h1>
+    </template>
+  </Layout>
+</template>
+
+<style scoped>
+.page-title {
+  margin-top: -3.5rem;
+  margin-bottom: 0.5rem;
+  font-size: 2.25rem;
+  font-weight: 700;
+  line-height: 1.2;
+  letter-spacing: -0.01em;
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,12 @@
 import BlogTheme from "@sugarat/theme";
 import "./style.css";
+import Layout from "./Layout.vue";
 
 import Timeline from "./components/Timeline.vue";
 
 export default {
   ...BlogTheme,
+  Layout,
   enhanceApp(ctx) {
     BlogTheme.enhanceApp(ctx);
     ctx.app.component("Timeline", Timeline);
